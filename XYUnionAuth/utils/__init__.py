4659d0c999278e6dc9ed72e629e8898564cdf9af
XYCode <xycode-xyc@outlook.com>
2024-02-03 17:53:25 +0800

    修复无法在运行时中重新加载权限列表的错误

--- XYUnionAuth/utils/__init__.py
+++ XYUnionAuth/utils/__init__.py
@@ -5,10 +5,6 @@ import casbin
 import casbin_pymongo_adapter
 from typing import Union, Dict
 
-adapter = casbin_pymongo_adapter.Adapter(
-    os.environ['MONGODB_URI'], os.environ['DATABASE_NAME'], 'polices')
-enforcer = casbin.Enforcer('casbin/abac_module.conf', adapter)
-
 
 def verify_token(token: str) -> Union[Dict[str, str], None]:
     try:
@@ -23,11 +19,17 @@ def verify_token(token: str) -> Union[Dict[str, str], None]:
 
 
 def check_permission(token: str, resource: str, action: str, environment_params: dict = {}) -> bool:
+    adapter = casbin_pymongo_adapter.Adapter(
+        os.environ['MONGODB_URI'], os.environ['DATABASE_NAME'], 'polices')
+
+    enforcer = casbin.Enforcer('casbin/abac_module.conf', adapter)
+
     decoded_token = verify_token(token)
 
     if decoded_token is None:
         return False
-    
+
     request = {**decoded_token, **environment_params}
+    enforcer.adapter.load_policy()
 
     return enforcer.enforce(request, resource, action)
