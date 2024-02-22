5dd26ca3307a0fe29bb5c5971b45acd5344f4897
XYCode Kerman <xycode-xyc@outlook.com>
2024-02-22 23:16:54 +0800

    使用环境变量定义允许的HOSTS

--- XYUnionAuth/settings.py
+++ XYUnionAuth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = eval(os.environ.get('ALLOWED_HOSTS'))
 
 
 # Application definition
